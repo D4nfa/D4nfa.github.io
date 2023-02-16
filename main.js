222702fbd60fcfc2253ad2b769b437eef4d988b8
lilf4 <danielfandersen@hotmail.com>
2023-02-16 11:00:02 +0100

    finished function to load and display project information dynamically and added more functionallity with localization

--- main.js
+++ main.js
@@ -2,24 +2,20 @@ var isHidden = true;
 var lang = 'DK';
 var project;
 
-onLoad();
+
 
 
 function onLoad(){
-    fetch(`./Projects/projects.json`).then((response) => response.json().then((json) => loadProjects(json)));
+    localizePage();
+    loadProjects();
+    //Add selected class to img depicting currently selected language
+    
+    $(lang + '-FLAG').classList.add('selected');
     
 }
 
-function loadProjects(projectPaths){
-    for(let path in projectPaths){
-        fetch(`./Projects/${path}/info.json`).then((response) => response.json().then((json) => projectLoaded(json)));
-    }
-}
 
-function projectLoaded(json){
-    json = subKeys(projectTemplate, [json[lang], json['GENERAL']]);
-    $('projectList').innerHTML += json;
-}
+
 
 function MinMaxNav()
 {
@@ -38,3 +34,75 @@ function MinMaxNav()
     }
     isHidden = !isHidden;
 }
+
+
+//Project loading
+function deleteProjects(){
+    $('projectList').innerHTML = '';
+}
+
+function loadProjects(){
+    fetch(`./Projects/projects.json`).then((response) => response.json().then((json) => {
+        for(let path of json){
+            console.log(`fetching file ${path}`);
+            fetch(`./Projects/${path}/info.json`).then((response) => response.json().then((json) => projectLoaded(json)));
+        }
+    }));
+}
+
+function projectLoaded(json){
+    json = subKeys(projectTemplate, [json[lang], json['GENERAL']]);
+    $('projectList').innerHTML += json;
+}
+
+//Change language parameter and reload website
+function changeLang(nLang){
+    if(nLang == lang) return;
+    if(window.location.href.indexOf('?') < 0)
+    {
+        window.location.href += `?lang=${nLang}&`;
+    }
+    let urlParam = new URLSearchParams(window.location.href.slice(window.location.href.indexOf('?') + 1));
+    for(let param of urlParam.entries()){
+        switch(param[0]){
+            case 'lang':
+                window.location.href = window.location.href.replaceAll(`${param[0]}=${param[1]}&`, `${param[0]}=${nLang}&`);
+                break;
+        }
+    }
+}
+
+function localizePage(){
+    fetch(`./local/homePage.json`).then((response) => response.json().then((json) => {
+        let page = document.getElementsByTagName('html')[0].innerHTML
+        page = subKeys(page, [json[lang], 'GENERAL']); 
+        document.getElementsByTagName('html')[0].innerHTML = page;
+    }));
+
+    
+}
+
+
+//Evaluate url params
+function evalParams(params)
+{
+    if (!params || window.location.href.indexOf('?') < 0) 
+    {
+        changeLang(lang);
+        return;
+    }
+
+    let qstr = new URLSearchParams(params);
+    for(let param of qstr.entries())
+    {
+        console.log(`${param[0]}${param[1]}`);
+        switch(param[0])
+        {
+            //change website language
+            case 'lang':
+                lang = param[1];
+                break;
+        }
+    }
+}
+evalParams(window.location.href.slice(window.location.href.indexOf('?') + 1));
--- templates.js
+++ templates.js
@@ -1,7 +1,8 @@
-const projectTemplate = '<li>'
+const projectTemplate = '<li id = "projectLi">'
 			+ '<div id = "project" >'
 			+ '<p>{TITLE}</p>'
 			+ '<p>{DESCRIPTION}</p>'
 			+ '<p>{INFO}</p>'
 			+ '</div>'
 			+ '</li>';
+
