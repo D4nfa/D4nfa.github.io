ebd3c29cbd2ba85987af69395a33e5db07e8c880
lilf4 <danielfandersen@hotmail.com>
2023-02-17 12:02:24 +0100

    Fixed indirect delay effect from using transition-delay instead of specifying a property

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var project;
 
 
 function onLoad(){
+    getLang();
     localizePage();
     loadProjects();
     //Add selected class to img depicting currently selected language
@@ -14,6 +15,10 @@ function onLoad(){
     
 }
 
+function getLang(){
+    let _lang = localStorage.getItem('lang');
+    if(_lang) {lang = _lang}
+}
 
 function scrollToAnchor(anchor)
 {
@@ -22,8 +27,8 @@ function scrollToAnchor(anchor)
 
 function MinMaxNav()
 {
-    var btn = $("navBtn");
-    var div = $("navDiv");
+    let btn = $("navBtn");
+    let div = $("navDiv");
     switch (isHidden) {
         case true:
             btn.classList.remove("navHidden");
@@ -63,19 +68,9 @@ function projectLoaded(json){
 //Change language parameter and reload website
 function changeLang(nLang){
     if(nLang == lang) return;
-    if(window.location.href.indexOf('?') < 0)
-    {
-        window.location.href += `?lang=${nLang}&`;
-    }
-    let urlParam = new URLSearchParams(window.location.href.slice(window.location.href.indexOf('?') + 1));
-    for(let param of urlParam.entries()){
-        switch(param[0]){
-            case 'lang':
-                localStorage.setItem('scrollpos', window.scrollY);
-                window.location.href = window.location.href.replaceAll(`${param[0]}=${param[1]}&`, `${param[0]}=${nLang}&`);
-                break;
-        }
-    }
+
+    localStorage.setItem('lang', nLang);
+    location.reload();
 }
 
 function localizePage(){
@@ -94,20 +89,14 @@ function evalParams(params)
 {
     if (!params || window.location.href.indexOf('?') < 0) 
     {
-        changeLang(lang);
         return;
     }
 
     let qstr = new URLSearchParams(params);
     for(let param of qstr.entries())
     {
-        console.log(`${param[0]}${param[1]}`);
         switch(param[0])
         {
-            //change website language
-            case 'lang':
-                lang = param[1];
-                break;
         }
     }
 }
