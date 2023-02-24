d36a1f6af47347394f0cf1786be5b73828a5b6b0
Lilly <47436048+D4nfa@users.noreply.github.com>
2023-02-24 14:45:23 +0100

    changed indentation to be tabs instead of spaces

--- localization.js
+++ localization.js
@@ -5,57 +5,57 @@ var lang = getLang();
 //Function that substitues keys in a text with key value pairs
 //Expects keys to be in an array to allow for use of multiple keysets
 function subKeys(template, keys) {
-    for(let keyset of keys){
-        for (let key of Object.keys(keyset)) {
-            template = template.replaceAll(`{${key}}`, keyset[key]);
-        }
-    }
-    return template;
+	for(let keyset of keys){
+		for (let key of Object.keys(keyset)) {
+			template = template.replaceAll(`{${key}}`, keyset[key]);
+		}
+	}
+	return template;
 }
 
 function subKeysDoc(doc, keys){
-    let nodes = doc.querySelectorAll('[localize]');
-    for(let keyset of keys){
-        for (let key of Object.keys(keyset)) {
-            for(let node of nodes){
-                node.innerHTML = node.innerHTML.replaceAll(`{${key}}`, keyset[key]);
-            }
-        }
-    }
+	let nodes = doc.querySelectorAll('[localize]');
+	for(let keyset of keys){
+		for (let key of Object.keys(keyset)) {
+			for(let node of nodes){
+				node.innerHTML = node.innerHTML.replaceAll(`{${key}}`, keyset[key]);
+			}
+		}
+	}
 }
 
 function subKeysAtr(doc, keys){
-    let nodes = doc.querySelectorAll('[localize]');
-    for(let keyset of keys){
-        for (let key of Object.keys(keyset)) {
-            for(let node of nodes){
-                if(node.getAttribute('localize') != key) continue;
-                node.innerHTML = keyset[key];
-            }
-        }
-    }
+	let nodes = doc.querySelectorAll('[localize]');
+	for(let keyset of keys){
+		for (let key of Object.keys(keyset)) {
+			for(let node of nodes){
+				if(node.getAttribute('localize') != key) continue;
+				node.innerHTML = keyset[key];
+			}
+		}
+	}
 }
 
 function getLang(){
-    if(localStorage.getItem('lang') != undefined){
-        return localStorage.getItem('lang');
-    }
-    return 'DK';
+	if(localStorage.getItem('lang') != undefined){
+		return localStorage.getItem('lang');
+	}
+	return 'DK';
 }
 
 //Change language parameter and reload website
 function changeLang(nLang){
-    if(nLang == lang) return;
-    localStorage.setItem('lang', nLang);
+	if(nLang == lang) return;
+	localStorage.setItem('lang', nLang);
 
-    document.getElementById(lang + '-FLAG').classList.remove('selected');
-    document.getElementById(nLang + '-FLAG').classList.add('selected');
+	document.getElementById(lang + '-FLAG').classList.remove('selected');
+	document.getElementById(nLang + '-FLAG').classList.add('selected');
 
-    lang = nLang;
-    this.dispatchEvent(changeEvent);
+	lang = nLang;
+	this.dispatchEvent(changeEvent);
 }
 
 function loadLangElement(){
-    document.getElementById('langElement').insertAdjacentHTML("afterend", langElement);
-    document.getElementById(lang + '-FLAG').classList.add('selected');
+	document.getElementById('langElement').insertAdjacentHTML("afterend", langElement);
+	document.getElementById(lang + '-FLAG').classList.add('selected');
 }
