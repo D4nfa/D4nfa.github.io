545ebd3bc1e52740570a0394292934b70528ef5d
lilf4 <danielfandersen@hotmail.com>
2023-03-03 12:45:44 +0100

    made it possible to draw and move/rotate boids

--- Projects/Games/Boids/script.js
+++ Projects/Games/Boids/script.js
@@ -1,17 +1,36 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
-let rot = 90 * (Math.PI/180);
+let rot = 90;
+
+let Boids = [];
+Boids.push({x: 100, y: 100, height: 40, rot: 0});
+Boids.push({x: 200, y: 200, height: 40, rot: 0});
+Boids.push({x: 300, y: 300, height: 40, rot: 0});
+Boids.push({x: 400, y: 400, height: 40, rot: 0});
+Boids.push({x: 500, y: 500, height: 40, rot: 0});
+Boids.push({x: 600, y: 600, height: 40, rot: 0});
+Boids.push({x: 700, y: 700, height: 40, rot: 0});
+let tickInterval = setInterval(doTick, 10);
+
+let speed = 10;
 
 
-let tickInterval = setInterval(doTick, 100);
 
 function doTick(){
     clearScreen();
-    drawTriangle(200, 200, 20, rot);
-
-    rot += 0.05;
-    if(rot > 6.28 ){rot = 0; console.log('full revolution');}
+    
+    Boids.forEach(boid => {
+        boid.rot++;
+        boid.x += (speed * 0.1) * getDir(degreeToRan(boid.rot)).x;
+        boid.y += (speed * 0.1) * getDir(degreeToRan(boid.rot)).y;
+        if(boid.x > canvas.width){boid.x -= canvas.width;}
+        if(boid.x < 0){boid.x += canvas.width;}
+        if(boid.y > canvas.height){boid.y -= canvas.width;}
+        if(boid.y < 0){boid.y += canvas.height;}
+        if(boid.rot > 360 ){boid.rot = 0;}
+        drawTriangle(boid.x, boid.y, boid.height, degreeToRan(boid.rot));
+    });
 }
 
 function clearScreen(){
@@ -47,8 +66,18 @@ function drawTriangle(_x, _y, _height, _rot, col)
     
     ctx.fillStyle = 'white';
     
-    ctx.fillRect(point1.x, point1.y, _height / 4, _height / 4);
+    ctx.fillRect(point1.x, point1.y, 1, 1);
     
     ctx.fillStyle = 'orange';
     ctx.fillRect(middle.x, middle.y, 1, 1)
 }
+
+
+//turn degrees into it's radians equiviliant
+function degreeToRan(degrees){ return degrees * Math.PI / 180; }
+
+//turn radians into it's degrees equiviliant
+function ranToDegree(radians){ return radians * 180 / Math.PI; }
+
+//get direction from radians
+function getDir(radians){ return {x: Math.cos(radians), y: Math.sin(radians)}; }
