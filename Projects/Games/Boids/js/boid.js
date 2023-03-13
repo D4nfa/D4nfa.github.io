67b52b45655f619250dac3a5c17bd9e4e1b5ec6b
lilf4 <danielfandersen@hotmail.com>
2023-03-13 14:56:23 +0100

    .

--- Projects/Games/Boids/js/boid.js
+++ Projects/Games/Boids/js/boid.js
@@ -1,107 +1,115 @@
-class Boid{
-	constructor(pos = new vector2()){
-		this.pos = pos;
-		this.vel = new vector2(Math.ceil(Math.random() * 10) * (Math.round(Math.random()) ? 1 : -1), Math.ceil(Math.random() * 10) * (Math.round(Math.random()) ? 1 : -1));
-		this.maxAccel = 0.012;
-		this.accel = new vector2();
-		this.height = 10;
-		this.col = 'green';
-		this.rot = 0;
-	}
+class Boid {
+  constructor(pos = new vector2()) {
+    this.pos = pos;
+    this.vel = new vector2(
+      Math.ceil(Math.random() * 10) * (Math.round(Math.random()) ? 1 : -1),
+      Math.ceil(Math.random() * 10) * (Math.round(Math.random()) ? 1 : -1)
+    );
+    this.maxAccel = 0.012;
+    this.accel = new vector2();
+    this.height = 10;
+    this.col = "green";
+    this.rot = 0;
+  }
 
-	applyLogic(){
-		this.screenWrap();
-		this.orient();
+  applyLogic() {
 
-		this.accel = this.accel.mult(0);
+    this.accel = this.accel.mult(0);
 
-		let sep = this.seperate(20).mult(sepForce.value);;
-		this.accel = this.accel.addVect(sep);
+    let sep = this.seperate(50).mult(sepForce.value);
+    this.accel = this.accel.addVect(sep);
 
-		let coh = this.cohesion(20).mult(atractForce.value);;
-		this.accel = this.accel.addVect(coh);
+    let coh = this.cohesion(50).mult(atractForce.value);
+    this.accel = this.accel.addVect(coh);
 
-		let align = this.align(20).mult(alignForce.value);;
-		this.accel = this.accel.addVect(align);
+    let align = this.align(50).mult(alignForce.value);
+    this.accel = this.accel.addVect(align);
 
-		this.accel = this.accel.setMag(0.5);
-	}
+    this.screenWrap();
+    this.orient();
+  }
 
-	screenWrap(){
-		if (this.pos.x > canvas.width) {
-			this.pos.x -= canvas.width;
-		}
-		if (this.pos.x < 0) {
-			this.pos.x += canvas.width
-		}
-		if (this.pos.y > canvas.height) {
-			this.pos.y -= canvas.height;
-		}
-		if (this.pos.y < 0) {
-			this.pos.y += canvas.height;
-		}
-	}
-	
-	orient(){
-		let angle = Math.atan2(this.vel.y, this.vel.x);
-    	angle = (angle < 0) ? (radToDegree(angle) + 360) : radToDegree(angle);
-		this.rot = angle;
-	}
+  screenWrap() {
+    if (this.pos.x > canvas.width) {
+      this.pos.x -= canvas.width;
+    }
+    if (this.pos.x < 0) {
+      this.pos.x += canvas.width;
+    }
+    if (this.pos.y > canvas.height) {
+      this.pos.y -= canvas.height;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y += canvas.height;
+    }
+  }
 
-	update(){
-		this.vel = limitVect(this.vel, this.maxVel);
-		this.pos = this.pos.addVect(this.vel);
-		this.vel = this.vel.addVect(this.accel);
-	}
+  orient() {
+    let angle = Math.atan2(this.vel.y, this.vel.x);
+    angle = angle < 0 ? radToDegree(angle) + 360 : radToDegree(angle);
+    this.rot = angle;
+  }
 
-	align(range){
-		let boids = getBoidsInRange(this, range);
-		if(boids.length < 1){return new vector2();}
+  update() {
+    this.vel = limitVect(this.vel, this.maxVel);
+    this.pos = this.pos.addVect(this.vel);
+    this.vel = this.vel.addVect(this.accel);
+  }
 
-		let avgVect = new vector2(0, 0);
-		boids.forEach(_boid => {
-			avgVect = avgVect.addVect(_boid.vel);
-		});
-		avgVect = avgVect.div(boids.length);
+  align(range) {
+    let boids = getBoidsInRange(this, range);
+    if (boids.length < 1) {
+      return new vector2();
+    }
 
-		avgVect = avgVect.subVect(this.vel);
+    let avgVect = new vector2(0, 0);
+    boids.forEach((_boid) => {
+      avgVect = avgVect.addVect(_boid.vel);
+    });
+    avgVect = avgVect.div(boids.length);
 
-		return limitVect(avgVect, this.maxAccel);
-	}
+    avgVect = avgVect.subVect(this.vel);
 
-	seperate(range){
-		let boids = getBoidsInRange(this, range);
-		if(boids.length < 1){return new vector2();}
+    return limitVect(avgVect, this.maxAccel);
+  }
 
-		let avgVect = new vector2(0, 0);
-		boids.forEach(_boid => {
-			let diff = this.pos.subVect(_boid.pos);
-			
-			diff = diff.div(getDist(this, _boid));
-			avgVect = avgVect.addVect(diff);
-		});
-		avgVect = avgVect.div(boids.length);
+  seperate(range) {
+    let boids = getBoidsInRange(this, range);
+    if (boids.length < 1) {
+      return new vector2();
+    }
 
-		avgVect = avgVect.subVect(this.vel);
-	
-		return limitVect(avgVect, this.maxAccel);
-	}
+    let avgVect = new vector2(0, 0);
+    boids.forEach((_boid) => {
+      let diff = this.pos.subVect(_boid.pos);
 
-	cohesion(range){
-		let boids = getBoidsInRange(this, range);
-		if(boids.length < 1){return new vector2();}
+      diff = diff.div(getDist(this, _boid));
+      avgVect = avgVect.addVect(diff);
+    });
+    avgVect = avgVect.div(boids.length);
 
-		let avgPos = new vector2(0, 0);
-		boids.forEach(_boid => {
-			avgPos = avgPos.addVect(_boid.pos);
-		});
-	
-		avgPos = avgPos.div(boids.length)
-	
-		avgPos = avgPos.subVect(this.pos);
-	
-		avgPos = avgPos.subVect(this.vel);
-	
-		return limitVect(avgPos, this.maxAccel);
-	}
+    avgVect = avgVect.subVect(this.vel);
+
+    return limitVect(avgVect, this.maxAccel);
+  }
+
+  cohesion(range) {
+    let boids = getBoidsInRange(this, range);
+    if (boids.length < 1) {
+      return new vector2();
+    }
+
+    let avgPos = new vector2(0, 0);
+    boids.forEach((_boid) => {
+      avgPos = avgPos.addVect(_boid.pos);
+    });
+
+    avgPos = avgPos.div(boids.length);
+
+    avgPos = avgPos.subVect(this.pos);
+
+    avgPos = avgPos.subVect(this.vel);
+
+    return limitVect(avgPos, this.maxAccel);
+  }
 }
--- Projects/Games/Boids/js/script.js
+++ Projects/Games/Boids/js/script.js
@@ -14,31 +14,31 @@ let tickInterval = setInterval(doTick, 10);
 spawnBoids(100);
 
 function doTick(){
-    clearScreen();
-    flock.forEach(boid => {
+	clearScreen();
+	flock.forEach(boid => {
 		boid.applyLogic();
-        boid.update();
-        drawBoid(boid);
+		boid.update();
+		drawBoid(boid);
 
-        
-        //BOID BE SPINNIN
-        /*
-        boid.rot++;
-        boid.x += (speed * 0.1) * getDir(degreeToRad(boid.rot)).x;
-        boid.y += (speed * 0.1) * getDir(degreeToRad(boid.rot)).y;
-        if(boid.x > canvas.width){boid.x -= canvas.width;}
-        if(boid.x < 0){boid.x += canvas.width;}
-        if(boid.y > canvas.height){boid.y -= canvas.width;}
-        if(boid.y < 0){boid.y += canvas.height;}
-        if(boid.rot > 360 ){boid.rot = 0;}
-        */
-    });
+		
+		//BOID BE SPINNIN
+		/*
+		boid.rot++;
+		boid.x += (speed * 0.1) * getDir(degreeToRad(boid.rot)).x;
+		boid.y += (speed * 0.1) * getDir(degreeToRad(boid.rot)).y;
+		if(boid.x > canvas.width){boid.x -= canvas.width;}
+		if(boid.x < 0){boid.x += canvas.width;}
+		if(boid.y > canvas.height){boid.y -= canvas.width;}
+		if(boid.y < 0){boid.y += canvas.height;}
+		if(boid.rot > 360 ){boid.rot = 0;}
+		*/
+	});
 }
 
 function spawnBoids(amount){
-    for(let i = 0; i < amount; i++){
-        flock.push(new Boid(new vector2(Math.random() * canvas.width, Math.random() * canvas.height)));
-    }
+	for(let i = 0; i < amount; i++){
+		flock.push(new Boid(new vector2(Math.random() * canvas.width, Math.random() * canvas.height)));
+	}
 }
 
 let sepForce = document.getElementById('seperation');
@@ -46,78 +46,51 @@ let atractForce = document.getElementById('cohesion');
 let alignForce = document.getElementById('align')
 let viewRange = 20;
 
-
-function boidLogic(boid){
-    
-
-
-    if(boidInView.length == 1){
-        return;
-    }
-
-    boid.accel = new vector2();
-    //SEPERATION
-    let sepVal = seperation(boidInView, boid);
-    sepVal = sepVal.mult(sepForce.value);
-    boid.accel = boid.accel.addVect(sepVal);
-
-    //ALIGNMENT
-    let alignVal = align(boidInView, boid);
-    alignVal = alignVal.mult(alignForce.value);
-    boid.accel = boid.accel.addVect(alignVal);
-
-    //COHESION
-    let attractVal = cohesion(boidInView, boid);
-    attractVal = attractVal.mult(atractForce.value);
-    boid.accel = boid.accel.addVect(attractVal);
-    
-}
-
 function getBoidsInRange(origin, range){
-    let boidsInRange = [];
-    for(let boid of flock){
-        if(boid != origin && getDist(origin, boid) <= range + boid.height){
+	let boidsInRange = [];
+	for(let boid of flock){
+		if(	boid != origin && getDist(origin, boid) <= range + boid.height){
 			boidsInRange.push(boid);
-        }
-    }
-    return boidsInRange;
+		}
+	}
+	return boidsInRange;
 }
 
 function getDist(boid1, boid2){ return Math.sqrt(Math.pow(boid2.pos.y - boid1.pos.y, 2) + Math.pow(boid2.pos.x - boid1.pos.x, 2)); }
 
 function clearScreen(){
-    ctx.clearRect(0, 0, 800, 800);
+	ctx.clearRect(0, 0, 800, 800);
 }
 
 function drawBoid(boid)
 {
-    let angle = 90;
-    let middle = {x: boid.pos.x, y: boid.pos.y};
-    let point1 = {
-        x: (boid.height / 2) * Math.cos(degreeToRad(boid.rot)) + middle.x,
-        y: (boid.height / 2) * Math.sin(degreeToRad(boid.rot)) + middle.y,
-    };
-    let point2 = {
-        x: ((point1.x - middle.x) * Math.cos(angle) - (point1.y - middle.y) * Math.sin(angle)) + middle.x,
-        y: ((point1.y - middle.y) * Math.cos(angle) + (point1.x - middle.x) * Math.sin(angle)) + middle.y
-    };
-    let point3 = {
-        x: ((point1.x - middle.x) * Math.cos(-angle) - (point1.y - middle.y) * Math.sin(-angle)) + middle.x,
-        y: ((point1.y - middle.y) * Math.cos(-angle) + (point1.x - middle.x) * Math.sin(-angle)) + middle.y
-    };
+	let angle = 90;
+	let middle = {x: boid.pos.x, y: boid.pos.y};
+	let point1 = {
+		x: (boid.height / 2) * Math.cos(degreeToRad(boid.rot)) + middle.x,
+		y: (boid.height / 2) * Math.sin(degreeToRad(boid.rot)) + middle.y,
+	};
+	let point2 = {
+		x: ((point1.x - middle.x) * Math.cos(angle) - (point1.y - middle.y) * Math.sin(angle)) + middle.x,
+		y: ((point1.y - middle.y) * Math.cos(angle) + (point1.x - middle.x) * Math.sin(angle)) + middle.y
+	};
+	let point3 = {
+		x: ((point1.x - middle.x) * Math.cos(-angle) - (point1.y - middle.y) * Math.sin(-angle)) + middle.x,
+		y: ((point1.y - middle.y) * Math.cos(-angle) + (point1.x - middle.x) * Math.sin(-angle)) + middle.y
+	};
 
 
-    ctx.fillStyle = boid.col
-    ctx.beginPath();
-    ctx.moveTo(point1.x, point1.y);
-    ctx.lineTo(point2.x, point2.y);
-    ctx.lineTo(point3.x, point3.y);
-    ctx.closePath();
+	ctx.fillStyle = boid.col
+	ctx.beginPath();
+	ctx.moveTo(point1.x, point1.y);
+	ctx.lineTo(point2.x, point2.y);
+	ctx.lineTo(point3.x, point3.y);
+	ctx.closePath();
 
-    ctx.fill();
-    
-    ctx.fillStyle = 'white';
-    ctx.fillRect(point1.x, point1.y, 1, 1);
+	ctx.fill();
+	
+	ctx.fillStyle = 'white';
+	ctx.fillRect(point1.x, point1.y, 1, 1);
 }
 
 
