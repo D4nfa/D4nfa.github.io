95b69b832415050692b34ac5757ec7d62351c0c4
lilf4 <danielfandersen@hotmail.com>
2023-05-08 07:31:06 +0200

    added update all qol to python script

--- Projects/prjctDate.py
+++ Projects/prjctDate.py
@@ -1,29 +1,34 @@
-import os
-clear = lambda: os.system('cls')
+import os;
+clear = lambda: os.system('cls');
 
 import json;
-from pathlib import Path
-from datetime import datetime
+from pathlib import Path;
+from datetime import datetime;
 
 with open(Path(__file__).with_name('projects.json'), 'r') as openfile:
-    json_object = json.load(openfile);
+	json_object = json.load(openfile);
 
 while True:
-    clear()
-    print('-1 to save file')
-    i = 0;
-    for x in json_object:
-        print(i, x);
-        i += 1;
+	clear();
+	print('-1 to save file');
+	print('0 to update all projects');
+	i = 1;
+	for x in json_object:
+		print(i, x);
+		i += 1;
 
 
-    prjct = int(input('which project do you want to update?\n'))
-    if(prjct == -1):
-        with open(Path(__file__).with_name('projects.json'), 'w') as outfile:
-            json.dump(json_object, outfile)
-        
-        break;
-    else:
-        userInput = input('are you sure you want to update ' + json_object[prjct]['link'] + '? y,n: ')
-        if(userInput == 'y'):
-            json_object[prjct]['lastUpdated'] = datetime.timestamp(datetime.now())
+	prjct = int(input('which project do you want to update?\n'))
+	if(prjct == -1):
+		with open(Path(__file__).with_name('projects.json'), 'w') as outfile:
+			json.dump(json_object, outfile);
+		break;
+	elif(prjct == 0):
+		userInput = input('are you sure you want to update all projects? y,n: ');
+		if(userInput == 'y'):
+			for x in json_object:
+				x['lastUpdated'] = datetime.timestamp(datetime.now());
+	else:
+		userInput = input('are you sure you want to update ' + json_object[prjct - 1]['link'] + '? y,n: ')
+		if(userInput == 'y'):
+			json_object[prjct - 1]['lastUpdated'] = datetime.timestamp(datetime.now());
