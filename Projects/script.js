49d0e761036b8758fe9e77499796d76c94fca551
lilf4 <danielfandersen@hotmail.com>
2023-11-08 14:50:04 +0100

    added support for tag icons

--- Projects/script.js
+++ Projects/script.js
@@ -1,50 +1,58 @@
 var projects = [];
 var projectElems = [];
-
+var tags;
+//Object.keys(json).forEach(tag => {
 function onLoad(){
-    loadProjects().then(() => {
+	fetch(`./tags.json`).then((response) => response.json().then((json) =>{ 
+		tags = json;
+		
+		loadProjects().then(() => {
 
-        localizePage();
-    });
+			localizePage();
+		});
+	}));
     loadLangElement();
     addEventListener('langChanged', () => 
     {
         localizePage();
         localizeProjects();
     });
+	
+	
 }
 
 
 //PROJECT LOADING
 function loadProjects(){
-	return fetch(`./projects.json`).then((response) => response.json().then((json) => {
+	return fetch(`./projects.json`).then((response) => response.json().then(async (json) => {
 		let prjcts = [];
 
-		json.forEach(path => {
-			fetch(`./${path}/info.json`)
-			.then((response) => response.json()
-			.then((json) => prjcts.push(json)))
-		})
-        
-		check();
-
-		async function check(){
-			if(prjcts.length < json.length - 1){
-				window.setTimeout(check, 100);
-			}
-            else{
-                prjcts.forEach(project => {
-					projectLoaded(project);
-				});
-            }
+		for(let i = 0; i < json.length; i++){
+			let path = json[i];
+			prjcts.push(await getPrjct(path['link']));
 		}
+
+		async function getPrjct(prjct){
+			return await fetch(`./${prjct}/info.json`)
+			.then((response) => response.json()
+			.then((json) => { return json; }));
+		}
+
+		prjcts.forEach(project => {
+			projectLoaded(project);
+		});
 	}));
 }
 
 function projectLoaded(json){
     projects.push(json);
     document.getElementById('prjctContainer').innerHTML += subKeys(projectTemplate, [json['GENERAL']]);
-
+	
+	var tagContainer = document.querySelector(`[PRJCTID=${json['GENERAL']['PRJCTID']}]`).getElementsByClassName(`tagDiv`)[0];
+	json['GENERAL']['TAGS'].forEach(element => {
+		tagContainer.innerHTML += `<img src="${tags[element]['iconLink']}" style="width:25px;">`;
+	});
+	
 	projectElems.push(document.querySelector(`[PRJCTID=${json['GENERAL']['PRJCTID']}]`));
 
 	localizeProject(json);
